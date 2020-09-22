2fa96366bfad46a43a4e6f9695a3d9d6de024f8c
shibayu36 <shibayu36@gmail.com>
2020-09-22 11:15:54 +0900

    EffectOrder

--- react-playground/src/App.tsx
+++ react-playground/src/App.tsx
@@ -1,8 +1,26 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { EffectOrder } from "./EffectOrder";
+
+const Home: React.FC = () => {
+  console.log("Home rendering");
+  return (
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        Learn React
+      </a>
+      <Link className="App-link" to="/effect-order">
+        effect order
+      </Link>
+    </header>
+  );
+};
 
 const App: React.FC = () => {
   return (
@@ -13,15 +31,7 @@ const App: React.FC = () => {
             <EffectOrder />
           </Route>
           <Route strict exact path="/">
-            <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <p>
-                Edit <code>src/App.tsx</code> and save to reload.
-              </p>
-              <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                Learn React
-              </a>
-            </header>
+            <Home />
           </Route>
         </Switch>
       </Router>
--- react-playground/src/EffectOrder.tsx
+++ react-playground/src/EffectOrder.tsx
@@ -1,20 +1,72 @@
-import React from "react";
+import React, { useEffect, useLayoutEffect } from "react";
+import { Link } from "react-router-dom";
 
 export const EffectOrder: React.FC = () => {
-  return <div>EffectOrder</div>;
+  console.log("EffectOrder rendering");
+  useEffect(() => {
+    console.log("EffectOrder useEffect");
+    return () => {
+      console.log("EffectOrder useEffect cleanup");
+    };
+  }, []);
+  return (
+    <>
+      <Link to="/">Home</Link>
+      <div>
+        <p>EffectOrder Component</p>
+        <Effect />
+      </div>
+    </>
+  );
 };
 
 const Effect: React.FC = () => {
   console.log("Effect rendering");
-  return <div>Effect Component</div>;
+  useEffect(() => {
+    console.log("Effect useEffect");
+    return () => {
+      console.log("Effect useEffect cleanup");
+    };
+  }, []);
+  return (
+    <div>
+      <p>Effect Component</p>
+      <LayoutEffect />
+    </div>
+  );
 };
 
 const LayoutEffect: React.FC = () => {
   console.log("LayoutEffect rendering");
-  return <div>LayoutEffect Component</div>;
+  useLayoutEffect(() => {
+    runSec(2);
+    console.log("LayoutEffect useLayoutEffect");
+
+    return () => {
+      runSec(2);
+      console.log("LayoutEffect useLayoutEffect cleanup");
+    };
+  }, []);
+  return (
+    <div>
+      <p>LayoutEffect Component</p>
+      <Child />
+    </div>
+  );
 };
 
 const Child: React.FC = () => {
   console.log("Child rendering");
+  useEffect(() => {
+    console.log("Child useEffect");
+    return () => {
+      console.log("Child useEffect cleanup");
+    };
+  }, []);
   return <div>Child Component</div>;
 };
+
+function runSec(seconds: number): void {
+  const start = performance.now();
+  while (performance.now() - start < 1000 * seconds);
+}
