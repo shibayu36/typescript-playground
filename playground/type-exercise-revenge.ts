5c2a0e11d0e8c5504686e5bb785cff8c03cf2b0d
shibayu36 <shibayu36@gmail.com>
2020-10-08 10:11:17 +0900

    型演習にリベンジ(2)

--- playground/type-exercise-revenge.ts
+++ playground/type-exercise-revenge.ts
@@ -1,6 +1,100 @@
 // 問題: https://qiita.com/uhyo/items/e4f54ef3b87afdd65546
 // 回答: https://qiita.com/uhyo/items/0e7821ce494024c98da5
 
+/** 3-3. イベント */
+interface EventPayloads {
+  start: {
+    user: string;
+  };
+  stop: {
+    user: string;
+    after: number;
+  };
+  end: {};
+}
+
+class EventDischarger<E> {
+  emit<Ev extends keyof E>(eventName: Ev, payload: E[Ev]): void {
+    // 省略
+  }
+}
+
+// 使用例
+const ed = new EventDischarger<EventPayloads>();
+ed.emit("start", {
+  user: "user1",
+});
+ed.emit("stop", {
+  user: "user1",
+  after: 3,
+});
+ed.emit("end", {});
+
+// エラー例
+ed.emit("start", {
+  user: "user2",
+  after: 0,
+});
+ed.emit("stop", {
+  user: "user2",
+});
+ed.emit("foobar", {
+  foo: 123,
+});
+
+/** 3-2. Partial */
+type MyPartial<T extends {}> = {
+  [P in keyof T]?: T[P];
+};
+// 使用例
+/*
+ * T1は { foo?: number; bar?: string; } となる
+ */
+type T1 = MyPartial<{
+  foo: number;
+  bar: string;
+}>;
+/*
+ * T2は { hoge?: { piyo: number; } } となる
+ */
+type T2 = MyPartial<{
+  hoge: {
+    piyo: number;
+  };
+}>;
+
+/** 3-1. 配列からMapを作る */
+function mapFromArray<T extends {}, K extends keyof T>(
+  arr: T[],
+  key: K
+): Map<T[K], T> {
+  const result = new Map();
+  for (const obj of arr) {
+    result.set(obj[key], obj);
+  }
+  return result;
+}
+
+// 使用例
+const data = [
+  { id: 1, name: "John Smith" },
+  { id: 2, name: "Mary Sue" },
+  { id: 100, name: "Taro Yamada" },
+];
+const dataMap = mapFromArray(data, "id");
+/*
+dataMapは
+Map {
+  1 => { id: 1, name: 'John Smith' },
+  2 => { id: 2, name: 'Mary Sue' },
+  100 => { id: 100, name: 'Taro Yamada' }
+}
+というMapになる
+*/
+
+// エラー例
+mapFromArray(data, "age");
+
 /** 2-5. useState */
 type SetStateArg<T> = T | ((prev: T) => T);
 declare function useState<T>(initial: T): [T, (state: SetStateArg<T>) => void];
