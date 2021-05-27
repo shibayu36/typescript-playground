b499454c5c75976874b366c4a70617641beef1aa
shibayu36 <shibayu36@gmail.com>
2021-05-27 19:19:20 +0900

    catch内でreturnしたときの挙動をチェック

--- playground/try-catch-finally.ts
+++ playground/try-catch-finally.ts
@@ -1,8 +1,20 @@
 try {
   throw new Error("hoge");
 } catch {
-  throw new Error("fuga");
+  // throw new Error("fuga");
 } finally {
   console.log("finally");
 }
 console.log("finished");
+
+const tryCatchExample = (): string => {
+  try {
+    throw new Error("hoge");
+  } catch {
+    return "hoge";
+  } finally {
+    console.log("tryCatchExample finally");
+  }
+};
+
+tryCatchExample();
